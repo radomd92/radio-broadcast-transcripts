0112490c2a01a71b3e0236d86f45013bb6bbf161
radomd92 <radomd92@gmail.com>
2023-03-12 12:38:15 +0100

    add more intelligence in transcriptor

--- transcriptor.py
+++ transcriptor.py
@@ -1,25 +1,51 @@
+import os
 import sys
 import textwrap
 import whisper
+import time
 
 
 class Transcriptor:
 
-    CHARACTERS_PER_LINE = 120
+    CHARACTERS_PER_LINE = 80
 
-    def __init__(self, model_name='medium.en'):
-        self.model = whisper.load_model(model_name)
+    def __init__(self, model):
+        self.model = model
 
     def run(self, path):
+        out_file_path = path.replace('.mp3', '.txt')    
+        if os.path.exists(out_file_path):
+            print(f"Skipping {out_file_path}: file exists")
+            return
+
         result = self.model.transcribe(path)
         text = result["text"]
-        with open(path.replace('.mp3', '.txt'), 'w') as out_file:
+        with open(out_file_path, 'w') as out_file:
             out_file.write(f'TRANSCRIPTION: {path.upper()}\n')
             out_file.write('-' * self.CHARACTERS_PER_LINE + '\n')
             out_file.write('\n'.join(textwrap.wrap(text, width=self.CHARACTERS_PER_LINE)))
 
 
 if __name__ == '__main__':
-    file_path = sys.argv[1]
-    worker = Transcriptor()
-    worker.run(file_path)
+    file_paths = sys.argv[1:]
+    print(file_paths)
+    
+    model_name='large-v2'
+    #model_name='small.en'
+    t0 = time.time()
+    print(f"[{time.time() - t0}] Loading model...")
+    model = whisper.load_model(model_name)
+    print(f"[{time.time() - t0}] Loaded model...")
+    
+
+    worker = Transcriptor(model)
+    t0 = time.time()
+    for file_path in file_paths:
+        print(f"[{time.time() - t0}] Running on {file_path}...")        
+        try:
+            worker.run(file_path)
+        except Exception as error:
+            print(error)
+            continue
+        print(f"[{time.time() - t0}] Finished {file_path}...")
+        t0 = time.time()
